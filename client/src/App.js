252f02b4e42145033f604fb5a012e09cffdccab0
GrishinAnton <asegrishin@sberbank.ru>
2020-10-31 20:43:40 +0300

    init redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { LoginPage } from "./components/LoginPage/LoginPage";
 import { history } from "./utils";
 import { NotificationContainer } from "react-notifications";
+import { getPersons } from "./features/persons/persons.slice";
+import { useDispatch } from "react-redux";
 
 import { Main } from "./components/Main/Main";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPersons());
+  }, []);
+
   return (
     <>
       <Router history={history}>
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+
 import { BookRecomendation } from "../BookRecomendation/BookRecomendation";
 import { BookRecommendationCarusel } from "../BookRecommendationCarusel/BookRecommendationCarusel";
 import Container from "@material-ui/core/Container";
@@ -7,7 +7,6 @@ import { Header } from "./../Header/Header";
 import { Reccomend } from "../Reccomend/Reccomend";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import { getPersons } from "../../features/persons/persons.slice";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -17,12 +16,6 @@ const useStyles = makeStyles((theme) => ({
 
 export const Main = () => {
   const classes = useStyles();
-  const dispatch = useDispatch();
-  // const persons = useSelector((state) => state.persons);
-
-  useEffect(() => {
-    dispatch(getPersons());
-  }, []);
 
   return (
     <Container maxWidth="lg">
