64075611e7a66f6924c73cfb0f1551532af79877
GrishinAnton <asegrishin@sberbank.ru>
2020-10-31 15:25:30 +0300

    Подключил табы

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -3,12 +3,25 @@ import { BookRecomendation } from "../BookRecomendation/BookRecomendation";
 import { BookRecommendationCarusel } from "../BookRecommendationCarusel/BookRecommendationCarusel";
 import Container from "@material-ui/core/Container";
 import { Header } from "./../Header/Header";
+import { Reccomend } from "../Reccomend/Reccomend";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginBottom: "10px",
+  },
+}));
 
 export const Main = () => {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
       <Header />
-      <div>SLIDER</div>
+      <Typography variant="h4" component="h4" className={classes.title}>
+        Ваш персональный гид
+      </Typography>
+      <Reccomend />
       <BookRecomendation />
       <BookRecommendationCarusel />
       <div>Мероприятия</div>
---
+++ client/src/components/Reccomend/Reccomend.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: "100%",
+  },
+}));
+
+export const Reccomend = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Ноябрь" {...a11yProps(0)} />
+          <Tab label="Декабрь" {...a11yProps(1)} />
+          <Tab label="Январь" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0} dir={theme.direction}>
+        Ноябрь
+      </TabPanel>
+      <TabPanel value={value} index={1} dir={theme.direction}>
+        Декабрь
+      </TabPanel>
+      <TabPanel value={value} index={2} dir={theme.direction}>
+        Январь
+      </TabPanel>
+    </div>
+  );
+};
