63191688845332fc381c2e68b8b173af1a677bae
GrishinAnton <asegrishin@sberbank.ru>
2020-10-31 21:00:47 +0300

    Обоготил данные в редаксе

--- client/src/features/persons/persons.slice.js
+++ client/src/features/persons/persons.slice.js
@@ -1,7 +1,40 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apiGetPersons } from "./persons.service";
+import { randomNumber } from "./../../utils";
 
-export const getPersons = createAsyncThunk("user/persons", apiGetPersons);
+export const getPersons = createAsyncThunk("user/persons", async () => {
+  const result = await apiGetPersons();
+
+  const data = result.map((item, i) => {
+    return {
+      ...item,
+      ["Прочитано"]: [
+        ...item["Прочитано"].map((item) => {
+          return {
+            ...item,
+            selected: false,
+            raiting: randomNumber(1, 5),
+          };
+        }),
+      ],
+      ["Мероприятия"]: [
+        ...item["Мероприятия"].map((item) => {
+          return {
+            ...item,
+            selected: false,
+            raiting: randomNumber(1, 5),
+          };
+        }),
+      ],
+      ["Рекомендации"]: [
+        ...item["Рекомендации"].map((item) => {
+          return { ...item, selected: false, raiting: randomNumber(1, 5) };
+        }),
+      ],
+    };
+  });
+  return data;
+});
 
 const userSlice = createSlice({
   name: "persons",
@@ -11,6 +44,7 @@ const userSlice = createSlice({
   reducers: {},
   extraReducers: {
     [getPersons.fulfilled.toString()]: (state, action) => {
+      console.log(action, "action");
       state.persons = action.payload;
     },
   },
--- client/src/utils.js
+++ client/src/utils.js
@@ -58,3 +58,7 @@ export const notification = (messageType) => {
       break;
   }
 };
+
+export const randomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
