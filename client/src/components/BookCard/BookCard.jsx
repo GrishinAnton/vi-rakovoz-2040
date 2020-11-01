a45c77cbc6897b88d746589c5a622b5f17d19d1a
Ivan Nikulin <I.Nikulin@x5.ru>
2020-11-01 12:06:20 +0300

    fix rating

--- client/src/components/BookCard/BookCard.jsx
+++ client/src/components/BookCard/BookCard.jsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const BookCard = ({ book, bookId, removeHandler }) => {
+const BookCard = ({ book, bookId, removeHandler, rating }) => {
   const [showReactionButtons, setShowReactionButtons] = useState(false);
   const [isBookmarked, setIsBookmarked] = useState(false);
   const classes = useStyles();
@@ -61,7 +61,7 @@ const BookCard = ({ book, bookId, removeHandler }) => {
           </Typography>
           <Typography color="textSecondary" className="book__rating">
             Рейтинг:
-            <Rating name="read-only" value={randomNumber(0, 5)} readOnly />
+            <Rating name="read-only" value={rating} readOnly />
           </Typography>
         </CardContent>
         {showReactionButtons ? (
